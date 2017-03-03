3714204f97fffd3bf248cff18f066f45ccbf5553
Brody <stephen.brody@gmail.com>
2017-03-03 16:02:36 -0500

    Removed fetch for merge
    Removed the component did mount function for merge into master. Will be
    added as part of a new feature branch for events.

--- src/components/events/index.js
+++ src/components/events/index.js
@@ -2,15 +2,6 @@ import React from 'react';
 //import { ajaxPromise } from '../helpers';
 
 export class MeetupEvents extends React.Component {
-
-    componentDidMount(){
-        //ajaxPromise('GET', 'http://meetup-on-rails.herokuapp.com/events.json')
-        fetch('http://localhost:4000/events')
-            .then( res => res.json() )
-            .then( json => { this.props.setEvents(json) })
-            .catch(err => console.log(err));
-    }
-
     render() {
         return (
             <div>
