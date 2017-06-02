898709b05a70662f909140ca8a1e81481f4beaad
bjsawyer <bjsawyer@vt.edu>
2017-06-01 21:28:59 -0400

    Switched from p to h3

--- src/components/whats-brewing/index.js
+++ src/components/whats-brewing/index.js
@@ -16,7 +16,7 @@ const WhatsBrewingComponent = () => (
                 <Image src={computerImage} responsive alt="What's Brewing?" />
             </Col>
             <Col md={6}>
-                <p>While you’re waiting for our website to come to life, feast on some of the work we’ve already done. Check out our blog to see what’s brewing!</p>
+                <h3>While you’re waiting for our website to come to life, feast on some of the work we’ve already done. Check out our blog to see what’s brewing!</h3>
                 <Button>Go to the Excella Blog</Button>
             </Col>
         </Row>
