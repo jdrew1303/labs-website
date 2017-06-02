fefe059e3ed4250cbc23850fff06ae0241aabfeb
Sean Killeen <SeanKilleen@gmail.com>
2017-06-01 21:25:33 -0400

    create and map list of test events

--- src/components/static-events/index.js
+++ src/components/static-events/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { Grid, Row, Col, Image } from 'react-bootstrap';
 import calendarImage from '../../images/static-calendar.png';
+import { StaticEvent } from './static-event';
+import { StaticEventListItem } from './static-event-list-item';
 
 export class StaticMeetupEvents extends React.Component {
+
+    constructor()
+    {
+        super();
+
+    this.testEvents = [
+      new StaticEvent("Sean test", new Date(), "Test Location", "TestGroup", "blahblahblahblabhlabhlabhlabhkaslkdfhaldiufa", "http://seankilleen.com", true, true, true)
+    ]  
+    }
+
+  
   render(){
     return (
       <div>
@@ -14,6 +27,12 @@ export class StaticMeetupEvents extends React.Component {
             </Col>
             <Col md={8}>
               <h2>Col2</h2>
+                <ul>
+                    { this.testEvents.map(event => 
+                        <StaticEventListItem event={event} key={event.id} />
+                    )}
+                </ul>
+
             </Col>
           </Row>
         </Grid>
