63a66ee3f4767dd09cb86739db9839c76b7aed93
Sean Killeen <SeanKilleen@gmail.com>
2017-06-01 20:19:23 -0400

    basic 4/8 grid

--- src/components/static-events/index.js
+++ src/components/static-events/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 export class StaticMeetupEvents extends React.Component {
   render(){
     return (
       <div>
         <h1>Meet Ups and Events</h1>
+        <Grid>
+          <Row>
+            <Col md={4}>
+              <h2>Col1</h2>
+            </Col>
+            <Col md={8}>
+              <h2>Col2</h2>
+            </Col>
+          </Row>
+        </Grid>
       </div>
     )
   }
