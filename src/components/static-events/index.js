3b3448f1130cded91f72e4c8e92728a01ef7feda
Sean Killeen <SeanKilleen@gmail.com>
2017-06-01 22:07:33 -0400

    use static event dat

--- src/components/static-events/index.js
+++ src/components/static-events/index.js
@@ -11,7 +11,10 @@ export class StaticMeetupEvents extends React.Component {
         super();
 
     this.testEvents = [
-      new StaticEvent("Sean test", new Date(), "Test Location", "blahblahblahblabhlabhlabhlabhkaslkdfhaldiufa", "http://seankilleen.com", true, true, true,123)
+      // TODO: Real links
+      new StaticEvent("Five Years of NoVA UX: A Look Back and a Look Ahead", new Date(2017,6,14,19,0,0), "Ellucian - Reston, VA", "Join us for a special celebration of ﬁve years of NoVA UX events. The NoVA UX team will be taking a look back at some of the best events since 2008", "http://SeanKilleen.com",false, false, false),
+      new StaticEvent("DC Full Stack Web Summit", new Date(2017,6,23,9,0,0), "Arlington Tech eXchange - Arlington, VA", "Full day technical conference about web-scale best practices on Coding, Agile, DevOps and UI/UX with a theme of Legacy System Modernization", "http://SeanKilleen.com",true, true, true),
+      new StaticEvent("The STEM + Art Forum", new Date(2017,6,28,17,0,0), "TBD", "The STEM+ Art Forum is an opportunity to harness cretive thinkers to discuss how Art and Tech can illuminate and deepen the value of technological", "http://SeanKilleen.com",true, true, true),
     ]  
     }
 
