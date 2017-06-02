f58834577e9c7b37188c2f04f1615b77de847a79
bjsawyer <bjsawyer@vt.edu>
2017-06-01 20:21:58 -0400

    Fixed "What's Brewing" sectoin

--- src/components/whats-brewing/index.js
+++ src/components/whats-brewing/index.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Button, Grid, Row, Col } from 'react-bootstrap';
-import computerImage from '../../images/computer-photo.jpeg'
+import computerImage from '../../images/computer-photo.jpeg';
+import './theme.css';
 
 const WhatsBrewingComponent = () => (
-    <Grid>
-        <Row>
-            <Col md={8}>
-                <h1>What's Brewing</h1>
-            </Col>
-        </Row>
-        <Row>
-            <Col md={4}>
-                <img src={computerImage} style={{width: '100%'}} />
-            </Col>
-            <Col md={4}>
-                <p>While you’re waiting for our website to come to life, feast on some of the work we’ve already done. Check out our blog to see what’s brewing!</p>
-                <Button>Go to the Excella Blog</Button>
-            </Col>
-        </Row>
-    </Grid>
+    <div className="section">
+        <Grid>
+            <Row>
+                <Col md={8}>
+                    <h1>What's Brewing</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col md={4}>
+                    <img src={computerImage} style={{width: '100%'}} />
+                </Col>
+                <Col md={4}>
+                    <p>While you’re waiting for our website to come to life, feast on some of the work we’ve already done. Check out our blog to see what’s brewing!</p>
+                    <Button>Go to the Excella Blog</Button>
+                </Col>
+            </Row>
+        </Grid>
+    </div>    
 );
 
 export default WhatsBrewingComponent;
