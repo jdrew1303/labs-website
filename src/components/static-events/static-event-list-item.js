a75f44f27667a92e92056fa2ca32d38d751a3996
Sean Killeen <SeanKilleen@gmail.com>
2017-06-01 20:36:41 -0400

    Copy the event list item class

---
+++ src/components/static-events/static-event-list-item.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export class StaticEventListItem extends React.Component {
+
+    static propTypes = {
+        event: React.PropTypes.object
+    };
+
+    render () {
+        const event = this.props.event;
+
+        return (
+            <li>
+                <a href={event.event_url} target="_blank" >{ event.event_name }</a><br />
+                { event.group_name }
+            </li>
+        );
+    }
+}
