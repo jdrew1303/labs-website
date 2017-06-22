0e7233a4acfead429d0c75858f4fe81d8c96fa6a
Keith Center <keith.center@excella.com>
2017-06-21 20:31:59 -0400

    Added section header component

---
+++ src/components/section-header/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import './theme.css';
+
+export default class Divider extends React.Component {
+  render() {
+    return (
+      <Grid style={{marginTop:'100px', marginBottom:'40px'}}>
+        <Row>
+          <Col sm={12} className="border">
+            <Row>
+              <Col sm={9}>
+                <h1 className="section-title">{this.props.sectionTitle}</h1>
+              </Col>
+              {this.props.linkURL 
+                ? <Col sm={3} style={{position: 'relative', height: '50px'}}>
+                    <a className="section-link" href={this.props.linkURL}>{this.props.linkText ? this.props.linkText : this.props.linkURL}</a>
+                  </Col>
+                : null
+              }
+            </Row>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
