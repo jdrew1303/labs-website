69b98ece67426363fd89919217dbfd93f3efbfbb
Keith Center <keith.center@excella.com>
2017-06-21 22:43:02 -0400

    Work in progress on footer.  Pushing to just to colaborate.

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,8 +6,24 @@ export class Footer extends React.Component {
     return (
       <div className="footer" style={{margin:'50px 0 0 0'}}>
         <div className="container">
-          <p><a href="https://twitter.com/excellaco" target="_blank">@excellaco</a></p>
-          <p><a href="https://www.excella.com" target="_blank">www.excella.com</a></p>
+          <div className="column">
+            <div>
+              <label>Stay in touch:</label>
+              <input type="text" />
+            </div>
+            <div>
+              <p> Like what you see here? </p>
+              <p> Check out our <a href="">opportunities</a>.</p>
+            </div>
+          </div>
+          <div className="column">
+            <div>
+              <a href="https://twitter.com/excellaco"><i className='fa fa-twitter' aria-hidden='true'></i></a>
+              <a href="https://www.facebook.com/excellaconsulting/"><i className='fa fa-facebook' aria-hidden='true'></i></a>
+              <a href="https://github.com/excellalabs"><i className='fa fa-github' aria-hidden='true'></i></a>
+              <p><a href="https://www.excella.com" target="_blank">www.excella.com</a></p>
+            </div>
+          </div>
         </div>
       </div>
     )
