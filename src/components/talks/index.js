f4ae7a99dfc3e82c4f2c79d1cb379b99c56ec0ff
5arias <stephen.brody@gmail.com>
2017-07-26 23:42:18 -0400

    fix: mobile responsive video embed

--- src/components/talks/index.js
+++ src/components/talks/index.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import { Button, Grid, Row, Col, Badge } from 'react-bootstrap'
+import { Button, Grid, Row, Col, Badge, ResponsiveEmbed } from 'react-bootstrap'
 import './theme.css'
 
 const Talks = () => (
   <div id="talks">
     <Grid>
         <Row>
-            <Col md={5}>
+            <Col md={7} mdPush={5} className="video-embed">
+              <ResponsiveEmbed a16by9>
+                <iframe src="https://www.youtube.com/embed/IQr5STWB_HM?rel=0&amp;showinfo=0" frameBorder="0" allowFullScreen style={{border: 0, padding: 0, margin: 0}}></iframe>
+              </ResponsiveEmbed>
+            </Col>
+            <Col md={5} mdPull={7}>
                 <h2>Angular vs React: An Out-of-the-Box Comparison</h2>
 
                 <h3 className="speakerName"><a href="https://www.excella.com/insights/author/doguhan-uluca" target="_blank" className="eventTitle">DOGUHAN ULUCA</a></h3>
@@ -17,9 +22,6 @@ const Talks = () => (
                     <Badge>CODING</Badge>
                 </div>
                 <Button bsStyle="primary" href="https://www.youtube.com/watch?v=IQr5STWB_HM&feature=youtu.be" target='_blank'>watch it here <i className='fa fa-arrow-right' aria-hidden='true'></i></Button>
-            </Col>
-            <Col md={7}>
-                <iframe width="640" height="360" src="https://www.youtube.com/embed/IQr5STWB_HM?rel=0&amp;showinfo=0" frameBorder="0" allowFullScreen style={{border: 0, padding: 0, margin: 0}}></iframe>
             </Col>
         </Row>
     </Grid>
